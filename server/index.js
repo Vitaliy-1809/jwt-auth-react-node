12ab5555490de2b2730e7aa07387f436a2bad26d
Vitaliy Gukalov <gukalov.v@ukr.net>
2022-01-04 13:46:54 +0200

    server ready

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+const router = require('./router')
+const errorMiddleware = require('./middlewares/error-middleware')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -10,6 +12,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
+app.use('/api', router)
+app.use(errorMiddleware)
 
 const start = async () => {
   try {
